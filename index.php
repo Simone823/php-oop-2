bd9732e044e49875bbf4570b498d4e244b69f70c
Simone823 <simone.daglio823@outlook.it>
2022-04-14 09:55:18 +0200

    product1, product2, order1, order2

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     require_once __DIR__ . "./user.php";
     require_once __DIR__ . "./userRegistered.php";
     require_once __DIR__ . "./creditCard.php";
+    require_once __DIR__ . "./product.php";
+    require_once __DIR__ . "./order.php";
 
 
 
@@ -14,4 +16,16 @@
 
     $userRegistered = new UserRegistered('Mario', 'Rossi', 30, 'uhgberyfe', 'jievbhe', $creditcard1);
     var_dump($userRegistered);
+
+    $product1 = new Product(1, 'Pallina', 'Animali', 'Giochi', 20);
+    var_dump($product1);
+
+    $product2 = new Product(2, 'Cuccia', 'Animali', 'Cura', 30);
+    var_dump($product2);
+
+    $order1 = new Order($product1);
+    var_dump($order1);
+
+    $order2 = new Order($product2);
+    var_dump($order2);
 ?>
